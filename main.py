864880d5214571cd609b7f0d0743391281a4f75a
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2021-12-06 14:10:18 +0500

    fix urlparse

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def shorten_link(token, url):
   return bitlink
 
 
-def count_clicks(token, link):
+def count_clicks(token, parsed_url):
   headers = {
     "Authorization": "Bearer {}".format(token) 
     }
-  url = "https://api-ssl.bitly.com/v4/bitlinks/{domain}{bitlink}/clicks/summary".format(domain=urlparse(link).netloc, bitlink=urlparse(link).path)
+  url = "https://api-ssl.bitly.com/v4/bitlinks/{domain}{bitlink}/clicks/summary".format(domain=parsed_url.netloc, bitlink=parsed_url.path)
   response = requests.get(url, headers=headers)
   response.raise_for_status()
   clicks_count = response.json()["total_clicks"]
@@ -39,13 +39,14 @@ def main():
   token = os.getenv("BITLY_TOKEN")
   args = parse_arguments()
   user_input = args.input_url
+  parsed_url = urlparse(user_input)
 
   try:
     bitlink = shorten_link(token, user_input)
     print(bitlink)
   except requests.exceptions.HTTPError:
     try:
-      clicks_count = count_clicks(token, user_input)
+      clicks_count = count_clicks(token, parsed_url)
       print(clicks_count)
     except requests.exceptions.HTTPError:
       print("Неверно введена ссылка")
